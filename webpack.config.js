d4814717751faccb32c2fc8ab1ae05e0cb30b42c
unknown <2844735407@qq.com>
2019-09-20 11:05:45 +0800

    A CSS资源管理模块学习

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,32 @@
 const path = require('path')
 
 module.exports = {
+    /**
+     * 入口
+     */
     entry: './src/index.js',
+    /**
+     * 输出
+     */
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    /**
+     * 管理资源
+     */
+    module: {
+        rules: [
+            /**
+             * 已.css结尾的全部文件，都将提供给 style-loader 和 css-loader
+             */
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
     }
 }
