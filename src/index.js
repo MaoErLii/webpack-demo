637f8cabd585cc7e5615d4f81ffff58ca0a8f764
unknown <2844735407@qq.com>
2019-09-30 14:17:57 +0800

    A 懒加载

--- src/index.js
+++ src/index.js
@@ -1,22 +1,47 @@
 // function getComponent() {
-async function getComponent() {
-    // console.log('component')
-    // return import('lodash').then(({ default: _ }) => {
-    //     const element =  document.createElement('div');
+// async function getComponent() {
+//     // console.log('component')
+//     // return import('lodash').then(({ default: _ }) => {
+//     //     const element =  document.createElement('div');
 
-    //     element.innerHTML = _.join(['Hello', 'Minato'], ' ');
+//     //     element.innerHTML = _.join(['Hello', 'Minato'], ' ');
         
-    //     return element;
-    // }).catch(error => 'An error occurred while loading the component');
+//     //     return element;
+//     // }).catch(error => 'An error occurred while loading the component');
     
-    var element = document.createElement('div');
-    const _ = await import('lodash');
+//     var element = document.createElement('div');
+//     const _ = await import('lodash');
 
-    element.innerHTML = _.join(['hello', 'aqua'], ' ');
+//     element.innerHTML = _.join(['hello', 'aqua'], ' ');
+
+//     return element;
+// }
+
+// getComponent().then(component => {
+//     document.body.appendChild(component);
+// })
+
+import _ from  'lodash';
+
+function component() {
+    var element = document.createElement('div');
+    
+    var btn = document.createElement('button');
+    var br = document.createElement('br');
+
+    btn.innerHTML = 'click me and check the console';
+
+    element.innerHTML = _.join(['hello', 'webpack'], '');
+    element.appendChild(br);
+    element.appendChild(btn);
+
+    btn.onclick = e => import('./print').then(module => {
+        var print = module.default;
+
+        print();
+    })
 
     return element;
 }
 
-getComponent().then(component => {
-    document.body.appendChild(component);
-})
+document.body.appendChild(component());
---
+++ src/print.js
@@ -0,0 +1,5 @@
+console.log('the print.js moudle has loaded! See the network tab in dev tools');
+
+export default() => {
+    console.log('Button Clicked: Here\'s "some test"!');
+}
