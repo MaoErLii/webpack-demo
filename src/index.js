b3ff09650168abe26d73c2a8ac8704e7d34b4e45
unknown <2844735407@qq.com>
2019-10-08 16:37:24 +0800

    A 添加sortablejs

--- src/index.js
+++ src/index.js
@@ -1,31 +1,19 @@
-// function getComponent() {
-// async function getComponent() {
-//     // console.log('component')
-//     // return import('lodash').then(({ default: _ }) => {
-//     //     const element =  document.createElement('div');
-
-//     //     element.innerHTML = _.join(['Hello', 'Minato'], ' ');
-        
-//     //     return element;
-//     // }).catch(error => 'An error occurred while loading the component');
-    
-//     var element = document.createElement('div');
-//     const _ = await import('lodash');
-
-//     element.innerHTML = _.join(['hello', 'aqua'], ' ');
-
-//     return element;
-// }
-
-// getComponent().then(component => {
-//     document.body.appendChild(component);
-// })
-
 import _ from  'lodash';
-// import Print from './print';
+import Sortable from 'sortablejs'
+import Print from './print';
 
 function component() {
     var element = document.createElement('div');
+
+    var ul = document.createElement('ul');
+
+    ul.id = 'test';
+
+    for(let i = 0; i < 3; i ++) {
+        var li = document.createElement('li');
+        li. innerHTML = 'item' + i
+        ul.appendChild(li);
+    }
     
     var btn = document.createElement('button');
     var br = document.createElement('br');
@@ -35,8 +23,11 @@ function component() {
     element.innerHTML = _.join(['hello', 'webpack'], '');
     element.appendChild(br);
     element.appendChild(btn);
+    element.appendChild(ul);
 
-    // btn.onclick = Print.bind(null, 'Hello webpack!');
+    btn.onclick = Print.bind(null, 'Hello webpack!');
+
+    var sortable = Sortable.create(ul);
 
     return element;
 }
